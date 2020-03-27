27d17e1c78a0ff7caa4b27762495a9dcd2a1b020
JOHNKUMAR-S <62602448+JOHNKUMAR-S@users.noreply.github.com>
2020-03-27 19:04:08 +0530

    Update OpencartTestRunner.java

--- Cucumber/src/test/java/TestRunner/OpencartTestRunner.java
+++ Cucumber/src/test/java/TestRunner/OpencartTestRunner.java
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)  //To run with cucumber class
 @CucumberOptions(
 		features = {"Feature\\My Test.Feature"},
 		plugin = {"pretty", "html:reports/cucumber-html-report","json:reports/cucumber-html-report/jsonreport","com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"},
 		tags = {"@TC_01_Opencart,@TC_02_Opencart,@TC_03_Opencart,@TC_04_Opencart,@TC_05_Opencart,@TC_06_Opencart"},
-		glue = {"stepDefinition"},
+		glue = {"stepDefinition"}, //Name of step defnition
 		monochrome = true
 		)
 
